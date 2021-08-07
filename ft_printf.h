77c5959bbdc6bfedf624d2ebc7e2aad0a69d6bcc
joaovgr <joaovictorgrodrigues@gmail.com>
2021-08-07 20:39:30 -0300

    add .h

--- ft_printf.h
+++ ft_printf.h
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvictor- <jvictor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/07 19:46:03 by jvictor-          #+#    #+#             */
-/*   Updated: 2021/08/07 20:12:28 by jvictor-         ###   ########.fr       */
+/*   Created: 2021/08/07 20:04:57 by jvictor-          #+#    #+#             */
+/*   Updated: 2021/08/07 20:28:26 by jvictor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include "./libft/libft.h"
+# include <stdarg.h>
 
-int ft_printf(const char *print, ...)
+typedef struct s_types
 {
-    
-}
+    char            c_char;
+    char            *s_string;
+    size_t          p_hex;
+    int             di_int;
+    unsigned int    u_uint;
+    unsigned int    xX_uint;
+} t_types;
+
+int ft_printf(const char *, ...);
+
+#endif
